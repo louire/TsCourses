7a76e352554f9c817be8eadefc17e329ec98ab4e
Loui Recio <lreciopersonal@gmail.com>
2023-11-02 23:48:51 -0500

    day 3

---
+++ TypescriptFundamentals/src/03-demo.ts
@@ -0,0 +1,13 @@
+(() => {//Funcion anonima autoinvocada
+    let productName: string = 'iPhone 5';
+
+    let productPrize: number = 900;
+
+
+
+    console.log("Mi nombre es Loui");
+
+
+
+
+});
---
+++ TypescriptFundamentals/src/03-typing.ts
@@ -0,0 +1,18 @@
+(() => { //Funcion Anonima Autoinvocada
+let productName: string = 'iPhone 5';
+
+let productPrize: number = 900;
+
+
+productName = "change";
+
+productName.repeat(2);
+
+productPrize.toString();
+
+})();
+
+
+
+
+
